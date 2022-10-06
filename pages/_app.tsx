6df6e9dcfb97cc8931205fda0f0abb7e979cc068
ikyro <ikyroch@gmail.com>
2022-10-06 00:04:56 -0300

    add context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { AuthProvider, User } from 'hooks/useAuth'
+import { TasksProvider } from 'hooks/useTasks'
+import { useForm, FormProvider } from 'react-hook-form'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<{ session: User }>) {
+  const methods = useForm()
+
+  return (
+    <AuthProvider session={session}>
+      <FormProvider {...methods}>
+        <TasksProvider>
+          <Component {...pageProps} />
+        </TasksProvider>
+      </FormProvider>
+    </AuthProvider>
+  )
 }
 
 export default MyApp
